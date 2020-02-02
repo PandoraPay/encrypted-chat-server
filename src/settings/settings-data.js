99fc023f3f44574aaae468b9a6d41c92e24a444a
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-02-02 23:58:03 +0200

    main settings update

---
+++ src/settings/settings-data.js
@@ -0,0 +1,72 @@
+export default class BaseChainData extends DBSchema {
+
+    constructor(scope, schema = {}, data, type, creationOptions) {
+
+        super(scope, Helper.merge({
+
+                fields: {
+
+                    table: {
+                        default: "settingsData",
+                        fixedBytes: 12,
+                    },
+
+                    version: {
+                        type: "number",
+                        default: 0,
+
+                        position: 100,
+                    },
+
+                    /**
+                     * Number of messages
+                     */
+                    index: {
+                        type: "number",
+                        default: 0,
+
+                        position: 101,
+                    },
+
+                    timestamp: {
+                        type: "number",
+                        default: 0,
+
+                        position: 102,
+                    },
+
+                    target: {
+
+                        type: "buffer",
+
+                        // minsize: 0,
+                        // maxsize: 2^256,
+
+                        position: 103,
+                    },
+
+
+                }
+
+            },
+            schema, false), data, type, creationOptions);
+
+    }
+
+    async loadData(){
+
+        if ( await this.exists() )
+            await this.load();
+
+        return true;
+    }
+
+    async clearData(){
+
+        if ( await this.exists() )
+            await this.delete();
+
+    }
+
+
+}
